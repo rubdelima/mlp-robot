36a67f887aa03c3ca2a04778e1dcc9f994d904b9
Jessyca Ferreira <ferreiradasilvajessyca@gmail.com>
2025-04-10 23:40:05 -0300

    feat: add new narx implementation

--- utils/narxwithga.py
+++ utils/narxwithga.py
@@ -2,12 +2,16 @@ import torch.nn as nn
 
 # Define NARX model
 class NARXModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim=10, output_dim=2):
+    def __init__(self, input_dim, hidden_dim=3, output_dim=2):
         super(NARXModel, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, output_dim)
         self.relu = nn.ReLU()
+        self.network = nn.Sequential(
+            self.fc1,
+            self.relu,
+            self.fc2
+        )
 
     def forward(self, x):
-        x = self.relu(self.fc1(x))
-        return self.fc2(x)
+        return self.network(x)
